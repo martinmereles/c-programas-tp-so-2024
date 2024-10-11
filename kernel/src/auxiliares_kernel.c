92c1ca1ba0c57baae2bcf3340512b8003a2d75c7
martinmereles <martinmereles@github.com>
2024-10-11 21:59:47 +0000

    avances sobre finalizacion de proceso

--- kernel/src/auxiliares_kernel.c
+++ kernel/src/auxiliares_kernel.c
@@ -58,3 +58,27 @@ void crear_proceso (char* archivo, int tamanio, int prioridad){
 
 }
 
+void finalizar_proceso (t_pcb* pcb;) {
+    //Cambio de estado el pcb del proceso
+    pcb->estado = EXIT;
+    //es necesario validar que los hilos esten finalizados? consultar
+
+       bool _es_pcb_buscado(void *elemento)
+    {
+        return es_pcb_buscado(atoi(pid), elemento);
+    }
+    t_pcb* pcb_encontrado;
+    pcb_encontrado = list_remove_by_condition(QUEUE_BLOCKED, _es_pcb_buscado);
+    list_add (QUEUE_EXIT, pcb_encontrado);
+    
+
+
+}
+
+bool es_pcb_buscado(int pid_buscado, void *elemento)
+{
+    t_pcb* aux = malloc(sizeof(t_pcb));
+    aux = elemento;
+    bool aux2 = (aux->pid == pid_buscado);
+    return (aux2);
+}
